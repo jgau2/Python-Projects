fca3063eae570d932ca26d3e6a42911184e37bf5
Joe Gaudreau <90278670+jgau2@users.noreply.github.com>
2022-05-12 14:43:51 -0400

    Update data_manager.py

--- flight-deal/data_manager.py
+++ flight-deal/data_manager.py
@@ -9,7 +9,7 @@ class DataManager:
         self.destination_data = {}
 
     def get_destination_data(self):
-        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        #Sheety API to GET all the data in that sheet and print it out.
         response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
         self.destination_data = data["prices"]
@@ -17,8 +17,8 @@ class DataManager:
         # pprint(data)
         return self.destination_data
 
-    # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
-    # to update the Google Sheet with the IATA codes. (Do this using code).
+    #In the DataManager Class make a PUT request and use the row id from sheet_data
+    #to update the Google Sheet with the IATA codes
     def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {
